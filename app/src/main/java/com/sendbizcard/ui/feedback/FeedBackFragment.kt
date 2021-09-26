a618e01dcbb64f80c5cd3303a0bd2a017e0b672b
kanhaiya <jsnkanhaiya@gmail.com>
2021-09-26 18:32:18 +0530

    Added MainActivity

--- app/src/main/java/com/sendbizcard/ui/feedback/FeedBackFragment.kt
+++ app/src/main/java/com/sendbizcard/ui/feedback/FeedBackFragment.kt
@@ -4,56 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.sendbizcard.databinding.FeedbackFragmentBinding
+import com.sendbizcard.base.BaseFragment
+import com.sendbizcard.databinding.FragmentFeedbackBinding
 
-class FeedBackFragment : Fragment() {
+class FeedbackFragment : BaseFragment<FragmentFeedbackBinding>() {
 
-    private val TAG = "FeedBackFragment"
+    private var binding: FragmentFeedbackBinding? = null
 
-    private lateinit var feedBackViewModel: FeedBackViewModel
-    private var _binding: FeedbackFragmentBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        feedBackViewModel =
-            ViewModelProvider(this).get(FeedBackViewModel::class.java)
-
-        _binding = FeedbackFragmentBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViews()
-        setupObservers()
+        binding = getViewBinding()
     }
 
-    private fun setupObservers() {
 
-    }
-
-    private fun initViews() {
-        binding.btnSave.setOnClickListener {
-            if (feedBackViewModel.isValidAllValue()){
-                feedBackViewModel.sendFeedBack()
-            }
-        }
-    }
-
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentFeedbackBinding
+        get() = FragmentFeedbackBinding::inflate
 }

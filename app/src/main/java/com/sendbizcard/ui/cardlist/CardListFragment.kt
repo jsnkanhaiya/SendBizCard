88937f045e3ef721fbb28a80b9e95f02e01859a8
kanhaiya <jsnkanhaiya@gmail.com>
2021-11-08 19:47:35 +0530

    data pass is edit

--- app/src/main/java/com/sendbizcard/ui/cardlist/CardListFragment.kt
+++ app/src/main/java/com/sendbizcard/ui/cardlist/CardListFragment.kt
@@ -63,8 +63,10 @@ class CardListFragment : BaseFragment<FragmentCardListBinding>() {
                 override fun onEditClicked(data: CardDetailsItem, pos: Int) {
                     Log.d("CardListFragment", "EditClickedCallback")
                     if (cardListViewModel.getThemeId() == "3"){
-                        val directions = CardListFragmentDirections.actionCardListFragmentToEditCardFragment(data,true)
-                        findNavController().navigate(directions)
+                        findNavController().navigate(R.id.nav_home, bundleOf("cardItem" to data, "isFromEditCard" to true),
+                            getDefaultNavigationAnimation())
+                        /*val directions = CardListFragmentDirections.actionCardListFragmentToEditCardFragment(data,true)
+                        findNavController().navigate(directions)*/
                     } else {
                         findNavController().navigate(R.id.nav_home_v2,null,
                             getDefaultNavigationAnimation())
